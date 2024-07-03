131023867a815aa73b226b3b49a5661d751e1830
esma <esmaoruccs@gmail.com>
2024-07-03 17:01:58 +0300

    Fixes #1: Add Task Edit Feature

---
+++ src/app/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-6xl font-bold text-slate-900">Welcome to your React Native app with Tailwind CSS!</h1>
+        <p className="text-xl text-slate-900 mt-5">Open up App.tsx to start working on your app!</p>
+        </div>
+    );
+    }
